1f432e86362640d51b21bc6764c738cdf7aeccf8
Art_Mainardi <iensenmainardi@gmail.com>
2026-07-09 23:13:18 -0300

    fix: ajusta links em 'dadosMissoes'

--- index-script.js
+++ index-script.js
@@ -4,28 +4,28 @@ const dadosMissoes = {
         desafio: "Desafio: Energia Potencial e Torque",
         descricao: "Projete um motor capaz de bombear água potável até o topo do Morro da Antena, vencendo a gravidade e a altitude elevada.",
         imagem: "imagens/chiesetta.jpg",
-        link: "missao/missao.html?id=1"
+        link: "/projetoFisica/missao/missao.html?id=1"
     },
     2: {
         nome: "Contrato #02: Parque Via Verde",
         desafio: "Desafio: Lei de Joule e Corrente",
         descricao: "Dimensionar o motor para os compressores das fontes de água. O sistema deve rodar por longos períodos sem superaquecer.",
         imagem: "imagens/via-verde.jpg",
-        link: "missao/missao.html?id=2"
+        link: "/projetoFisica/missao/missao.html?id=2"
     },
     3: {
         nome: "Contrato #03: Centro Cultural SCAR",
         desafio: "Desafio: Rotação e Ressonância",
         descricao: "Ajuste o motor do sistema de exaustão e climatização do grande teatro para o concerto comemorativo da cidade, evitando ruídos por vibração.",
         imagem: "imagens/scar.jpg",
-        link: "missao/missao.html?id=3"
+        link: "/projetoFisica/missao/missao.html?id=3"
     },
     4: {
         nome: "Contrato #04: Terminal Rodoviário",
         desafio: "Desafio: Inércia e Torque de Partida",
         descricao: "Projete o motor para as novas cancelas e portões de fluxo contínuo de ônibus. O motor precisa vencer a inércia rapidamente.",
         imagem: "imagens/rodoviaria.jpg",
-        link: "missao/missao.html?id=4"
+        link: "/projetoFisica/missao/missao.html?id=4"
     }
 };
 
